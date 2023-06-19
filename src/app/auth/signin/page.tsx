5353203aea5741d4b56123625088adbd6be5d29e
Soye Park <stardust6653@gmail.com>
2023-06-20 01:18:31 +0900

    feat : sign in 커스텀 페이지 내 스타일링 및 리다이렉트를 위한 getProvider함수 추가

--- src/app/auth/signin/page.tsx
+++ src/app/auth/signin/page.tsx
@@ -1,10 +1,30 @@
-import { authOptions } from "@/app/api/auth/[...nextauth]/route";
-import { getServerSession } from "next-auth";
 import React from "react";
 
-const SignPage = async () => {
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
+import { getProviders } from "next-auth/react";
+import SignIn from "@/components/SignIn";
+
+type Props = {
+  searchParams: {
+    callbackUrl: string;
+  };
+};
+
+const SignPage = async ({ searchParams: { callbackUrl } }: Props) => {
   const session = await getServerSession(authOptions);
-  return;
+  if (session) {
+    redirect("/");
+  }
+
+  const providers = (await getProviders()) ?? {};
+
+  return (
+    <section className="flex justify-center mt-24">
+      <SignIn providers={providers} callbackUrl={callbackUrl ?? "/"} />
+    </section>
+  );
 };
 
 export default SignPage;
