7f1a76c64748e0983b1dcf8b10d212bdcb722824
Soye Park <stardust6653@gmail.com>
2023-06-19 16:55:34 +0900

    feat : schema 추가
    - post
    - user

--- sanity-studio/sanity.config.ts
+++ sanity-studio/sanity.config.ts
@@ -8,7 +8,7 @@ const devOnlyPlugins = [getStartedPlugin()]
 
 export default defineConfig({
   name: 'default',
-  title: 'salmon-dove',
+  title: 'instagram-clone',
 
   projectId: 'xxy8ct4b',
   dataset: 'production',
@@ -19,4 +19,3 @@ export default defineConfig({
     types: schemaTypes,
   },
 })
-
