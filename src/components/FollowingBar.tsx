32fa569e5e24edf6c1f380cc3541bed1b60ac990
Soye Park <stardust6653@gmail.com>
2023-06-23 01:02:41 +0900

    feat : 팔로워 확인이 가능한 팔로잉바 마크업 및 스타일링

---
+++ src/components/FollowingBar.tsx
@@ -0,0 +1,48 @@
+"use client";
+
+import { DetailUser } from "@/model/user";
+import Link from "next/link";
+import React from "react";
+import { PropagateLoader } from "react-spinners";
+import useSWR from "swr";
+import Avatar from "./Avatar";
+import ScrollableBar from "./ui/ScrollableBar";
+
+const FollowingBar = () => {
+  const { data, isLoading: loading, error } = useSWR<DetailUser>("/api/me");
+
+  // const users = data?.following;
+  const users = data?.following && [
+    ...data?.following,
+    ...data?.following,
+    ...data?.following,
+  ];
+
+  return (
+    <section className="w-full flex justify-center items-center p-4 shadow-sm shadow-neutral-300 mb-4 rounded-lg min-h-[90px] overflow-x-auto">
+      {loading ? (
+        <PropagateLoader size={8} color="red" />
+      ) : (
+        (!users || users.length === 0) && <p>{`You don't have following`}</p>
+      )}
+      {users && users.length > 0 && (
+        <ScrollableBar>
+          {users.map(({ image, username }) => (
+            <Link
+              key={username}
+              href={`/user/${username}`}
+              className="flex flex-col items-center w-20"
+            >
+              <Avatar image={image} highlight />
+              <p className="w-full text-center text-sm text-ellipsis overflow-hidden">
+                {username}
+              </p>
+            </Link>
+          ))}
+        </ScrollableBar>
+      )}
+    </section>
+  );
+};
+
+export default FollowingBar;
