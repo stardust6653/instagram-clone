1177d8ff4d0e9ea59d2a12c59c8a6d2525a9c0d4
Soye Park <stardust6653@gmail.com>
2023-06-23 01:04:00 +0900

    feat : 사이드바 마크업 및 스타일링

---
+++ src/components/SideBar.tsx
@@ -0,0 +1,30 @@
+import { User } from "@/model/user";
+import React from "react";
+import Avatar from "./Avatar";
+
+type Props = {
+  user: User;
+};
+
+const SideBar = ({ user: { name, username, image } }: Props) => {
+  return (
+    <>
+      <div className="flex items-center">
+        {image && <Avatar image={image} />}
+        <div className="ml-4">
+          <p className="font-bold">{username}</p>
+          <p className="text-lg text-neutral-500 leading-4">{name}</p>
+        </div>
+      </div>
+
+      <p className="text-sm text-neutral-500 mt-8">
+        About﹒Help﹒Press﹒API﹒Jobs﹒Privacy﹒Terms﹒Location﹒Launguage
+      </p>
+      <p className="font-bold text-sm mt-8 text-neutral-500">
+        @Copyright Instagram from META
+      </p>
+    </>
+  );
+};
+
+export default SideBar;
