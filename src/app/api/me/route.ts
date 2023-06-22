101467fd22fdbf630fad8fde8a97ef624a0b4d2f
Soye Park <stardust6653@gmail.com>
2023-06-23 00:56:01 +0900

    feat : 로그인 유저 정보 불러오는 API 작성

---
+++ src/app/api/me/route.ts
@@ -0,0 +1,16 @@
+import { getServerSession } from "next-auth";
+import { NextResponse } from "next/server";
+import { authOptions, OPTIONS } from "../auth/[...nextauth]/route";
+import { getUserByUsername } from "@/service/user";
+
+export async function GET(request: Request) {
+  const session = await getServerSession(OPTIONS);
+  const user = session?.user;
+
+  if (!user) {
+    return new Response("Authentication Error", { status: 401 });
+  }
+  return getUserByUsername(user.username).then((data) =>
+    NextResponse.json(data)
+  );
+}
