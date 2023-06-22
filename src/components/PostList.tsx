24398d64ee00b733cabf74a038ee9c7ee42af10b
Soye Park <stardust6653@gmail.com>
2023-06-23 02:33:53 +0900

    feat : PostCard 마크업

--- src/components/PostList.tsx
+++ src/components/PostList.tsx
@@ -4,13 +4,32 @@ import useSWR from "swr";
 
 import React from "react";
 import { SimplePost } from "@/model/post";
+import { GridLoader } from "react-spinners";
+import PostListCard from "./PostListCard";
 
 const PostList = () => {
   const { data: posts, isLoading: loading } =
     useSWR<SimplePost[]>("/api/posts");
-
+  console.log(posts);
   return (
-    <ul>{posts && posts.map((post) => <li key={post.id}>{post.text}</li>)}</ul>
+    <section>
+      {loading && (
+        <div>
+          <GridLoader color="red" />
+        </div>
+      )}
+
+      {posts && (
+        <ul>
+          {posts &&
+            posts.map((post) => (
+              <li key={post.id}>
+                <PostListCard post={post} />
+              </li>
+            ))}
+        </ul>
+      )}
+    </section>
   );
 };
 
